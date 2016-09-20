7f67cb7067034ee18adb46adbc702e22382415af
dimpu47 <dimpu47@gmail.com>
2016-09-20 19:33:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/gaurav.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'gaurav.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/ui/cream.jpg', function (req, res) {
