9e10587cd8d25cc2efeb6a4e4f66fee66ced8a35
dimpu47 <dimpu47@gmail.com>
2016-09-24 00:33:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,15 +44,14 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('sbmit_btn');
 submit.onclick = function () {
-  // Make a reuest to the server and send the name
-  
-  // cature a list of name and render it as the list.
-  var names = ['name1', 'name3', 'name3', 'name4'];
-  var list = '';
-  for (var i=0; i<names.length;i++) {
+    // Make a reuest to the server and send the name
+    
+    // cature a list of name and render it as the list.
+    var names = ['name1', 'name3', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.length;i++) {
       list += '<li>' + names[i]+'</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-  
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
