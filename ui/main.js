f274fb9e0430123d3da760a785c93f7b67fa1a5d
dimpu47 <dimpu47@gmail.com>
2016-09-24 01:08:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ button.onclick = function () {
     request.send(null);
 };
 
-// SUbmit Name
+// Submit Name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
