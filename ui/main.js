ae9c83c629bd1efe6e2d29271a8cdbe4e2d76cc4
dimpu47 <dimpu47@gmail.com>
2016-09-23 20:37:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.InnerHTML = 'New Value';
+
+//madi's movement
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() { 
+    marginLeft = marginLeft+1; 
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () { 
+    var interval = setInterval(moveRight, 50);
+};
