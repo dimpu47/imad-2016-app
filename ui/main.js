52d3fbaa18ebb7b84025a9e248ecd39a4ee79ea3
dimpu47 <dimpu47@gmail.com>
2016-09-24 01:37:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ img.onclick = function () {
 var button = document.getElementById('counter');
 
 button.onclick = function () {
-    // create request to counter endpoint
+   // create request to counter endpoint
     var request = new XMLHttpRequest();
     // Capture the response and store it in a variable
     request.onreadystatechange = function () {
@@ -34,7 +34,7 @@ button.onclick = function () {
         }
     };
     
-    // make request
+    // make request 
     request.open('GET', 'http://dimpu47.imad.hasura-app.io/counter', true);
     request.send(null);
 };
@@ -44,14 +44,28 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    // Make a reuest to the server and send the name
+    // create request to counter endpoint
+    var request = new XMLHttpRequest();
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status===200) {
+                // cature a list of name and render it as the list.
+                var names = request.responseText;
+                names = JSON.parse(name);
+                var list = '';
+                for (var i=0; i<names.length;i++) {
+                  list += '<li>'+names[i]+'</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
     
-    // cature a list of name and render it as the list.
-    var names = ['name1', 'name3', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length;i++) {
-      list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // make request 
+    request.open('GET', 'http://dimpu47.imad.hasura-app.io/submit-name?name=' name, true);
+    request.send(null);
+    
+    
 };
