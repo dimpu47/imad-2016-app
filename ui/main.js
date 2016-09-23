22dec6eb956acb87f86cdbd72293915ec73c7d46
dimpu47 <dimpu47@gmail.com>
2016-09-24 00:36:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,7 @@ submit.onclick = function () {
     var names = ['name1', 'name3', 'name3', 'name4'];
     var list = '';
     for (var i=0; i<names.length;i++) {
-      list += '<li>' + names[i]+'</li>';
+      list += '<li>'+names[i]+'</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
