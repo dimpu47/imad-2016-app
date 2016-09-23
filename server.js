7e28d495ed38390fbb5b7b9949381fd2cbb871e5
dimpu47 <dimpu47@gmail.com>
2016-09-23 23:13:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ function createTemplate (data) {
 
 var counter=0;
 app.get('/counter', function(req,res){
- counter++;
- res.send(counter,toString());
+ counter = counter + 1;
+ res.send(counter.toString());
 });   
 
 app.get('/', function (req, res) {
