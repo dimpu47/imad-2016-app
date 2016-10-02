7973db348879b373926da6c86b7a6aac42b480aa
dimpu47 <dimpu47@gmail.com>
2016-10-03 04:07:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,12 +20,25 @@ var articles = {
         <p>
             This is the content of first article from the future. Guess what? There's No apocalypse ! You will live to die into Eternity.
         </p>
+        
         <!-- begin wwww.htmlcommentbox.com -->
-        <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>
+        
+        <div id="HCB_comment_box">
+            
+            <a href="http://www.htmlcommentbox.com">
+                HTML Comment Box</a>is loading comments...
+                
+        </div>
+        
         <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
-    <script type="text/javascript" id="hcb">
-    /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24M57RZnwCgxZ1t20Sl2zuO."+"&opts=16862&num=10&ts=1475447189920");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-<!-- end www.htmlcommentbox.com -->
+        
+        <script type="text/javascript" id="hcb">
+        
+         /*<!--*/ 
+         if(!window.hcb_user){hcb_user={};} (function() {var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24M57RZnwCgxZ1t20Sl2zuO."+"&opts=16862&num=10&ts=1475447189920");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ 
+        
+        </script>
+        <!-- end www.htmlcommentbox.com -->
         `
         
     },
