2eedf04e41176c8ad0d6a0f351ba902350e04b23
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-12 02:22:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@ submit.onclick = function () {
                 names = JSON.parse(names);
                 var list = '';
                 for (var i=0; i<names.length;i++) {
-                  list += '<li>'+names[i]+'</li>';
+                  list += '<li class="list-unstyled">'+names[i]+'</li>';
                 }
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
