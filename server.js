23366260193a242f9acbf744f2a1924a4822f38a
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-11 19:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,7 @@ app.get('/submit-name', function(req, res) {
 
 
 app.get('/articles/:articleName', function(req,res) {
+    
     // var articleName = req.params.articleName;
     
     pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName + "'", function (err, results) {
