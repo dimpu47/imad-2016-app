b90c1a8aa24a4f2cdc62f7920222b683eba8c44b
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-11 19:28:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,9 +141,22 @@ app.get('/submit-name', function(req, res) {
     // JSON
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName', function(req,res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req,res) {
+    // var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function (err, results) {
+      if (err) {
+          res.status(500).send(err, toString());
+      }  else {
+          if (results.rows.length === 0) {
+              res.status(404).send('Article Not Found :(');
+          } else {
+              var articleData = results.row[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+    });
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
