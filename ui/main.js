bd90e4ae1692d02b80ac239d32e86694dd1ff4cf
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-12 20:52:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -75,3 +75,43 @@ submit.onclick = function () {
     
     
 };
+
+// Submit Name
+var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+
+     var request = new XMLHttpRequest(); 
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+         if(request.status === 200){
+             
+             console.log('user loged in');
+             alert("Login Successfully");
+             window.location.href ='/info';
+         }else {
+             if(request.status==403){
+                 alert("Invalid password/Username");
+                 submit.value = 'Invalid credentials. Try again?';
+             }else {
+             if(request.status==500){
+                 alert("Internal Server Error");
+             }
+          }
+         }
+     }
+     
+ };
+ 
+ var username = document.getElementById('username').value;
+ var password = document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
+ request.open('POST','http://dimpu47.imad.hasura-app.io/login', true);
+ request.setRequestHeader('Content-Type','application/json');
+ request.send(JSON.stringify({username: username, password: password}));  
+ submit.value = 'Logging in...'; 
+   
+};
+    
+    
+};
