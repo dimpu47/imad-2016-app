f886cecc4be785b9b75b967a0bbd9e75db8c4247
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-13 02:40:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,9 +79,9 @@ submit.onclick = function () {
 // Submit Name
 var submit = document.getElementById('submit_btn1');
 submit.onclick= function(){
-
-     var request = new XMLHttpRequest(); 
-  request.onreadystatechange = function(){
+    
+    var request = new XMLHttpRequest(); 
+    request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE){
          if(request.status === 200){
              
@@ -102,13 +102,13 @@ submit.onclick= function(){
      
  };
  
- var username = document.getElementById('username').value;
- var password = document.getElementById('password').value;
- console.log(username);
- console.log(password);
- request.open('POST','http://dimpu47.imad.hasura-app.io/login', true);
- request.setRequestHeader('Content-Type','application/json');
- request.send(JSON.stringify({username: username, password: password}));  
- submit.value = 'Logging in...'; 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://dimpu47.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));  
+    submit.value = 'Logging in...'; 
    
 };
