cf77d905f0f49994f6556edbae3b600f7ab37ac4
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-21 02:10:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ button.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE){
             if (request.status===200) {
                 var counter = request.responseText;
-                var span = document.getElementById('count');
+                var span = document.getElementById('count2');
                 span.innerHTML = counter.toString();
             }
         }
