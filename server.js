ade45b82fcbd6f80dd5194031c89d6f8ed2df5f7
Gaurav Choudhary <dimpu47@gmail.com>
2016-11-21 19:10:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -195,7 +195,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout',function (req,res){
    delete req.session.auth;
-   res.send('<html><body></br><div align="center container container-fluid"><div class="jumbotron"><h3>Logged out!</h3><br/><br/><a href="/">Back to home</a> </div> </div></body></html>');
+   res.send('<html><body></br><div align="container container-fluid"><div class="jumbotron"><h1 class="lead">Logged out!</h1><br/><br/><a href="/">Back to home</a> </div> </div></body></html>');
     
 });
 
